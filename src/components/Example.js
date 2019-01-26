df45b57c30fd4587aae2c7bedcf98b9768ac4002
kitety <aizaizuori@foxmail.com>
2019-01-26 23:14:20 +0800

    跨域和css

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,14 +1,26 @@
 import React from "react";
-import './example.css'
+// import './example.css'
 import styles from './example.css'
+// import './example.less'
+import './example.scss'
+import axios from 'axios'
 
 const Example = () => {
-  return (
-    <div>
+  return <div>
       <h1>Hello</h1>
-      <div className={styles.cool}> Example test</div>
-    </div>
-  );
+      {/*  <div className={styles.cool}> Example test</div> */}
+      <div> Example test</div>
+      <button
+        onClick={() =>
+          axios.get("https://jsonplaceholder.typicode.com/users")
+        }
+      >
+        Ajax1
+      </button>
+      <button onClick={() => axios.get("/api/users")}>
+        Ajax2
+      </button>
+    </div>;
 };
 
 Example.propTypes = {};
